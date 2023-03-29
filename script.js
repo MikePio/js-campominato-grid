d1668b09576b9aab44ccf2a1948b13d355026eff
MikePio <michelepiopilla@gmail.com>
2023-03-30 00:33:31 +0200

    Added 2 difficulty (bonus with bugs on buttons)

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
-**Consigli del giorno:**  :party_wizard:
+**Consigli del giorno:** 
 Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
 Ad esempio:
 Di cosa ho bisogno per generare i numeri?
@@ -23,7 +23,9 @@ Le validazioni e i controlli possiamo farli anche in un secondo momento.
 
 // const arrayBoxes = [];
 
-const buttonGenerate = document.querySelector('.btn');
+const buttonEasy = document.querySelector('#easy-btn');
+const buttonNormal = document.querySelector('#normal-btn');
+const buttonHard = document.querySelector('#hard-btn');
 const mainWrapper = document.querySelector('.main-wrapper');
 
 
@@ -72,10 +74,10 @@ const mainWrapper = document.querySelector('.main-wrapper');
 
   
   
-  //* button che genera container e box
-  buttonGenerate.addEventListener('click', function(){
+//* button MODALITà FACILE che genera container e box-----------------------------------------------------------------------------------------------------------------------------
+buttonEasy.addEventListener('click', function(){
   // disabilito il button
-  buttonGenerate.disabled = true;
+  buttonEasy.disabled = true;
 
   // //per la generazione del container 
   // const generateContainer = document.createElement('div');
@@ -86,14 +88,15 @@ const mainWrapper = document.querySelector('.main-wrapper');
   generaContainer();
   
   for (let i = 0; i < 100; i++){
-
+    
     //per la generazione di delle box
     const generateBoxes = document.createElement('div');
     // //generazione di una box
     // generateBoxes.classList.add('square');
     // document.querySelector('.container').append(generateBoxes);
     //*OPPURE
-    generaBox(generateBoxes);
+    const squareType = generateBoxes.classList.add('square-smaller');
+    generaBox(generateBoxes, squareType);
 
     generateBoxes.addEventListener('click', function(){
       //* cliccando la prima cambia colore e la seconda ritorna come prima
@@ -108,13 +111,96 @@ const mainWrapper = document.querySelector('.main-wrapper');
       //* CREO UNA PROPRIETà 
       generateBoxes.boxid = i;
       console.log(generateBoxes);
-  });
-
+    });
+    
   }
   
 });
 
-console.log(buttonGenerate);
+console.log(buttonEasy);
+
+//* button DIFFICOLTà NORMALE che genera container e box---------------------------------------------------------------------------------------------------------------------------
+buttonNormal.addEventListener('click', function(){
+  // disabilito il button
+  buttonNormal.disabled = true;
+
+  // //per la generazione del container 
+  // const generateContainer = document.createElement('div');
+  // //generazione del container
+  // generateContainer.classList.add('container');
+  // mainWrapper.append(generateContainer);
+  //*OPPURE
+  generaContainer();
+  
+  for (let i = 0; i < 81; i++){
+    
+    //per la generazione delle box
+    const generateBoxes = document.createElement('div');
+    const squareType = generateBoxes.classList.add('square-normal');
+    generaBox(generateBoxes, squareType);
+    
+    generateBoxes.addEventListener('click', function(){
+      //* cliccando la prima cambia colore e la seconda ritorna come prima
+      generateBoxes.classList.toggle('clicked');
+      //OPPURE (NON LA MIGLIORE PERCHè NON è POSSIBILE LEGGERE LE PROPRIETà NEL CONSOLE.LOG)
+      // this.classList.toggle('clicked');
+      //* cliccando cambia colore per sempre
+      // generateBoxes.classList.add('clicked');
+      //OPPURE
+      // generateBoxes.style.backgroundColor = '#355289';
+      // console.log(generateBoxes);
+      //* CREO UNA PROPRIETà 
+      generateBoxes.boxid = i;
+      console.log(generateBoxes);
+    });
+    
+  }
+  
+});
+
+console.log(buttonNormal);
+
+//* button DIFFICOLTà DIFFICILE che genera container e box---------------------------------------------------------------------------------------------------------------------------
+buttonHard.addEventListener('click', function(){
+  // disabilito il button
+  buttonHard.disabled = true;
+  //TODO MODIFICA LA FUNZIONE generaContainer INSERENDO COME PARAMETRO CONTAINER E SUCCESSIVAMENTE CREA PER OGNI BUTTON LE SEGUENTI CONST
+  // const removeEasyContainer = document.querySelector('.container-easy').classList.add('hide'); 
+  // const removeNormalContainer = document.querySelector('.container-normal').classList.add('hide'); 
+  // const showNewContainer = document.querySelector('.container-hard').classList.add('show');
+  // buttonEasy.enabled = true;
+  // buttonNormal.enabled = true;
+
+  generaContainer();
+  
+  for (let i = 0; i < 49; i++){
+    
+    //per la generazione delle box
+    const generateBoxes = document.createElement('div');
+    const squareType = generateBoxes.classList.add('square-bigger');
+    generaBox(generateBoxes, squareType);
+    
+    generateBoxes.addEventListener('click', function(){
+      //* cliccando la prima cambia colore e la seconda ritorna come prima
+      generateBoxes.classList.toggle('clicked');
+      //OPPURE (NON LA MIGLIORE PERCHè NON è POSSIBILE LEGGERE LE PROPRIETà NEL CONSOLE.LOG)
+      // this.classList.toggle('clicked');
+      //* cliccando cambia colore per sempre
+      // generateBoxes.classList.add('clicked');
+      //OPPURE
+      // generateBoxes.style.backgroundColor = '#355289';
+      // console.log(generateBoxes);
+      //* CREO UNA PROPRIETà 
+      generateBoxes.boxid = i;
+      console.log(generateBoxes);
+    });
+    
+  }
+  
+});
+
+console.log(buttonHard);
+
 
 // * --------------------------- FUNZIONI ---------------------------------- /
 
@@ -126,9 +212,9 @@ function generaContainer(){
   mainWrapper.append(generateContainer);
 }
 
-function generaBox(newBoxes){
+function generaBox(newBoxes, addClassBoxes){
   //generazione di una box
-  newBoxes.classList.add('square');
+  addClassBoxes;
   document.querySelector('.container').append(newBoxes);
 }
 
